c922af0440abda549eac4b8ab7d445ce0d1bc1e1
Franck Mamboue <franckmamboue@Francks-MacBook-Pro.local>
2017-12-28 08:47:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/franckmamboue/myfliptransition/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.franckmamboue.myfliptransition
+
+import android.app.Fragment
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+
+            // Add blank layer
+            val blankHandler = Handler()
+            blankHandler.postDelayed({
+                container2.foreground = ContextCompat.getDrawable(this, R.color.white)
+            }, 150)
+
+            // Flip card
+            flipCard()
+
+            // Open profile activity
+            val handler = Handler()
+            handler.postDelayed({
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }, 300)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val handler = Handler()
+        handler.postDelayed({
+            // Remove card
+            fragmentManager
+                    .beginTransaction()
+                    .remove(fragmentManager.findFragmentById(R.id.container))
+                    .commit()
+
+            // Remove blank layer
+            container2.foreground = null
+
+        }, 100)
+    }
+
+    class CardBackFragment : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+            return inflater?.inflate(R.layout.activity_profile, container, false)!!
+        }
+    }
+
+    private fun flipCard() {
+
+        // Create and commit a new fragment transaction that adds the fragment for
+        // the back of the card, uses custom animations, and is part of the fragment
+        // manager's back stack.
+
+        fragmentManager
+                .beginTransaction()
+
+                // Replace the default fragment animations with animator resources
+                // representing rotations when switching to the back of the card, as
+                // well as animator resources representing rotations when flipping
+                // back to the front (e.g. when the system Back button is pressed).
+                .setCustomAnimations(
+                        R.animator.flip_right_in,
+                        R.animator.flip_right_out,
+                        R.animator.flip_left_in,
+                        R.animator.flip_left_out)
+
+                // Replace any fragments currently in the container view with a
+                // fragment representing the next page (indicated by the
+                // just-incremented currentPage variable).
+                .replace(R.id.container, CardBackFragment())
+
+                // Commit the transaction.
+                .commit()
+    }
+}
